49f4b23ebdceb0a82079b1c41f6682af86f47d70
vijay manikanta <vijayakp444@gmail.com>
2023-12-27 22:30:43 +0530

    defined window

--- src/app/_trpc/Provider.jsx
+++ src/app/_trpc/Provider.jsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import React, { useState } from "react";
@@ -7,15 +8,28 @@ import { trpc } from "./client";
 
 export default function Provider({ children }) {
   const [queryClient] = useState(() => new QueryClient({}));
-  const [trpcClient] = useState(() =>
-    trpc.createClient({
-      links: [
-        httpBatchLink({
-          url: `${window.location.href}api/trpc`,
-        }),
-      ],
-    })
-  );
+  const [trpcClient, setTrpcClient] = useState(null);
+
+  useEffect(() => {
+    // Check if we are running on the client side
+    if (typeof window !== "undefined") {
+      // Create trpc client with the correct URL
+      const client = trpc.createClient({
+        links: [
+          httpBatchLink({
+            url: `${window.location.href}api/trpc`,
+          }),
+        ],
+      });
+      setTrpcClient(client);
+    }
+  }, []); // Run this effect only once on component mount
+
+  // Render the component only if the trpcClient is available
+  if (!trpcClient) {
+    return null;
+  }
+
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
